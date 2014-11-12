f57ce73839da4c6f126af2a24d9b47baa30c9707
sicavz <sicavz@yahoo.com>
2014-11-12 10:28:09 +0200

    added output sample

--- ProgrammingSchool/Holidays/Program.cs
+++ ProgrammingSchool/Holidays/Program.cs
@@ -20,10 +20,33 @@ namespace Holidays
             };
 
             var holidayRequest = employee.AskForHoliday(new DateTime(2014, 11, 25), new DateTime(2014, 11, 28));
-
             manager.ApproveHoliday(holidayRequest);
-
             manager.RejectHoliday(holidayRequest);
         }
     }
 }
+
+/* Sample output
+ * 
+ *
+ * 
+Email sent
+        From    : employee@company.com
+        To      : manager@company.com
+        Subject : Employee asked a holiday for [25.11.2014 - 28.11.2014]
+        Body    : Please accept/reject the request
+
+Email sent
+        From    : manager@company.com
+        To      : hr@company.com
+        Subject : Manager approved a holiday for Employee btw [25.11.2014 - 28.11.2014]
+        Body    :
+
+Email sent
+        From    : manager@company.com
+        To      : employee@company.com
+        Subject : Manager rejected your holiday request btw [25.11.2014 - 28.11.2014]
+        Body    :
+ * 
+ * 
+ */
